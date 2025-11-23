70078258ff0be8d9bbd79dc6068afc35448dcc83
superpro2005 <159615396+superpro2005@users.noreply.github.com>
2025-11-23 23:17:15 +0300

    need ACF and spectre

--- task.py
+++ task.py
@@ -3,13 +3,16 @@ import matplotlib.pyplot as plt
 import random as rand
 import numpy.fft as fft
 
-Fb = 1023e6
+Fb = 1.023e6
 Fs = 4*Fb
 DURATION  = 0.020
 AMPLITUDE = 1
 IF =  Fs / 4
 LENGTH_P = 4092
 LENGTH_D = 1023
+OC_RATE = 500.0
+NAV_RATE = 125.0
+CC_RATE = 250.0
 
 t = np.arange(0.0,DURATION,1.0/Fs)
 N = t.size
@@ -23,7 +26,7 @@ def lfsr_sequence(taps,start_seq, length):
         seq.append(reg[-1])
         bit = 0
         for t in taps:
-            bit = bit ^reg[t]
+            bit = bit ^reg[-t]
         reg =  [bit] + reg[:-1]
     return np.array(seq)
 
@@ -32,6 +35,22 @@ def upsampl(seq,fb,fs,duration):
     spc = int(fs/fb)
     seq = np.repeat(seq,spc)
     return seq[:N]
+
+def make_mp(N):
+    return np.tile([0,1],N//2+2)[:N]
+
+def PVU(data_chips,pilot_chips):
+    total_chips = len(data_chips) + len(pilot_chips)
+    result = np.zeros(total_chips)
+
+
+    data_positions = np.arange(0, len(data_chips) * 2, 2)
+    result[data_positions] = data_chips[:len(data_positions)]
+
+    pilot_positions = np.arange(1, len(pilot_chips) * 4, 4)
+    result[pilot_positions[:len(pilot_chips)]] = pilot_chips
+
+    return result[result != 0]
 
 # ДК для L1OCp
 # из ИКД L1
@@ -53,4 +72,21 @@ L1OCd_sequence = 1-2*xor_sequences_d
 seq_p = upsampl(L1OCp_sequence,Fb,Fs,DURATION)
 seq_d = upsampl(L1OCd_sequence,Fb,Fs,DURATION)
 
+samples_nav = int(Fs/NAV_RATE)
+nav_bits = np.tile([1,0],int(np.ceil(N/samples_nav/2)))
+nav_seq =np.repeat(nav_bits,samples_nav)[:N]
+
+samples_cc = int(Fs/CC_RATE)
+CC_seq = np.repeat(nav_seq,2)[:N]
+
+samples_oc = int(Fs/OC_RATE)
+oc_seq = np.repeat([0,1],samples_oc)
+oc_seq = np.tile(oc_seq,  N // len(oc_seq) + 1)[:N]
+
+chips_mod = 1-2*(oc_seq^nav_seq)
+
+mp =1-2*make_mp(N)
+
+L1OCd=chips_mod^L1OCd_sequence
+L1OCp=mp^L1OCp_sequence
 
